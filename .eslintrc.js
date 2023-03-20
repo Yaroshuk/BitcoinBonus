664db4b3df82133378693c2547a7433253f0a554
Siarhei Yarashuk <siarhei.yarashuk@softswiss.com>
2023-03-21 00:23:49 +0400

    init

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    "plugin:react/recommended",
+    "standard"
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: [
+    "react"
+  ],
+  rules: {
+    quotes: ["error", "double"],
+    "jsx-quotes": [
+      "error",
+      "prefer-double"
+    ]
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react"
 import {
   ChakraProvider,
   Box,
@@ -7,18 +7,20 @@ import {
   VStack,
   Code,
   Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+  theme
+} from "@chakra-ui/react"
+import { ColorModeSwitcher } from "./ColorModeSwitcher"
+import { Logo } from "./Logo"
 
-function App() {
+function App () {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
+      <Box textAlign="center" fontSize="xl"
+      >
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
+
             <Logo h="40vmin" pointerEvents="none" />
             <Text>
               Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
@@ -36,7 +38,7 @@ function App() {
         </Grid>
       </Box>
     </ChakraProvider>
-  );
+  )
 }
 
-export default App;
+export default App
