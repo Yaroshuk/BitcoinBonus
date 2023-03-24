8cd774a144379cf11104437bd50ef7f89972952e
Siarhei Yarashuk <siarhei.yarashuk@softswiss.com>
2023-03-24 22:18:50 +0400

    feat: login modal step switching

--- src/components/Modals/LoginModal/index.js
+++ src/components/Modals/LoginModal/index.js
@@ -1,5 +1,13 @@
-import React, { useState } from "react"
-import { Box, Text, Image, Flex, Button } from "@chakra-ui/react"
+import React, { useCallback, useState } from "react"
+import {
+  Box,
+  Text,
+  Image,
+  Flex,
+  Button,
+  OrderedList,
+  ListItem
+} from "@chakra-ui/react"
 import { ArrowForwardIcon } from "@chakra-ui/icons"
 
 import CustomModal from "../CustomModal"
@@ -40,15 +48,19 @@ const hStyle = {
 
 const btnStyles = {
   fontWeight: "bold",
-  rightIcon: <ArrowForwardIcon fontSize={"2xl"} />,
   minW: { base: "190px", sm: "230px" },
   marginTop: "32px",
   alignSelf: { base: "center", md: "flex-start" }
 }
 
 const LoginModal = ({ isOpen }) => {
-  const [step, useStep] = useState(0)
+  const [step, setStep] = useState(1)
   const user = "user-id5673345" // TODO: username from store
+  const sum = "24 281,80" // TODO: sum from store
+
+  const handleNextStep = useCallback(() => {
+    setStep(step => ++step)
+  }, [setStep])
 
   const stepOne = () => (
     <Flex {...stepStyles}>
@@ -63,14 +75,75 @@ const LoginModal = ({ isOpen }) => {
           You were not active in your personal account, but the collection of
           cryptocurrency accrued automatically from your device.
         </Text>
-        <Button {...btnStyles}>NEXT</Button>
+        <Button
+          {...btnStyles}
+          rightIcon={<ArrowForwardIcon fontSize={"2xl"} />}
+          onClick={handleNextStep}
+        >
+          NEXT
+        </Button>
       </Flex>
     </Flex>
   )
 
+  const stepTwo = () => (
+    <Flex {...stepStyles}>
+      <Image {...iconStyles} src={"/img/icons/warning-2.svg"} />
+      <Flex {...contentStyle}>
+        <Text {...hStyle}>{`Dear ${user},`}</Text>
+        <Text {...textStyles}>
+          {`
+          During your absence you have made a sum of money with the help of
+          cloud mining in the amount of 1.31 BTC. that at the domestic exchange
+          rate at the end of the mining process on the March 16 is $ ${sum}`}
+          <br />
+          <br />
+          {`Your account will be deleted across 1 day ! You have not been active
+          on the site 364 days from the date of registration.`}
+        </Text>
+        <Button
+          {...btnStyles}
+          rightIcon={<ArrowForwardIcon fontSize={"2xl"} />}
+          onClick={handleNextStep}
+        >
+          NEXT
+        </Button>
+      </Flex>
+    </Flex>
+  )
+
+  const stepThree = () => (
+    <Flex {...stepStyles}>
+      <Image {...iconStyles} src={"/img/icons/warning-2.svg"} />
+      <Flex {...contentStyle}>
+        <Text {...hStyle}>{`Dear ${user},`}</Text>
+        <OrderedList {...textStyles} textAlign={"left"}>
+          <ListItem>
+            Log in to your account by clicking the button below. If you do not
+            remember your old password, then use the password that the system
+            will automatically give you.
+          </ListItem>
+          <ListItem>
+            In your personal account, contact your personal manager, they will
+            prepare a payment for you.
+          </ListItem>
+          <ListItem>Request a withdrawal.</ListItem>
+        </OrderedList>
+        <Button {...btnStyles}>Log In</Button>
+      </Flex>
+    </Flex>
+  )
+
+  const stepComponent = {
+    1: stepOne,
+    2: stepTwo,
+    3: stepThree,
+    default: stepOne
+  }
+
   return (
     <CustomModal isOpen={isOpen}>
-      {stepOne()}
+      {(stepComponent[step] ?? stepComponent.default)?.()}
       <Box>
         <YourBalance />
       </Box>
