3582f8ca86930ad16732724a5a57abb727013658
Siarhei Yarashuk <siarhei.yarashuk@softswiss.com>
2023-03-26 17:11:40 +0400

    fix: top mainers img

--- src/components/TopMainersList/index.js
+++ src/components/TopMainersList/index.js
@@ -5,25 +5,25 @@ import TopMainersItem from "../TopMainersItem"
 
 const list = [
   {
-    img: "/img/avatars/avatar_1.jpeg",
+    img: "./img/avatars/avatar_1.jpeg",
     nickname: "Wade Warren",
     link: "WWarenW",
     sum: "98 336.00"
   },
   {
-    img: "/img/avatars/avatar_2.jpeg",
+    img: "./img/avatars/avatar_2.jpeg",
     nickname: "Jack",
     link: "Jack_22",
     sum: "66 113.00"
   },
   {
-    img: "/img/avatars/avatar_3.jpeg",
+    img: "./img/avatars/avatar_3.jpeg",
     nickname: "Masterioo",
     link: "Masterioo",
     sum: "56 870.00"
   },
   {
-    img: "/img/avatars/avatar_4.jpeg",
+    img: "./img/avatars/avatar_4.jpeg",
     nickname: "Robert Fox",
     link: "fox_robe",
     sum: "55 336.00"
